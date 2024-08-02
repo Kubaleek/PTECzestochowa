27cc4efdb901436787c1350ef1fc43bfba504c32
Kuba Król <kuba.krol.ldz@gmail.com>
2024-08-02 23:49:22 +0200

    Wygenerowałem dla clienta nextjs

---
+++ client/tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
