ca18c100eff0bea1da1d4d5223a96c931031dc8b
Kuba Król <kuba.krol.ldz@gmail.com>
2024-08-03 10:48:12 +0200

    "Zaktualizowano plik globals.css o nowe style, zmodyfikowano plik układ.tsx, aby uwzględnić komponent nagłówka i zaktualizowano metadane"

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+import PTECzestochowaLogo from "../assets/PTECzęstochowa/Logo_PTE_poziome_Czestochowa_e271bd3c00.png";
+import PTESiedziba from "../assets/PTECzęstochowa/pte_siedziba.jpg";
+
+const Header = () => {
+  const HeaderBlock = () => {
+    return (
+      <>
+        <header className="bg-[#fff]">
+          <div className="max-w-[1440px] pt-0 lg:px-[10vw] hidden lg:flex mx-auto items-center justify-between">
+            <div>
+              <div className="grid grid-cols-2 gap-4">
+                <div className="flex items-center">
+                  <Link href={"/"} className="flex items-center">
+                    <Image src={PTECzestochowaLogo} alt="Logo PTE Częstochowa" loading = 'lazy'/>
+                  </Link>
+                </div>
+                <div className="flex justify-center">
+                  <span className="ml:[40px]">
+                    <Image src={PTESiedziba} alt="PTE Siedziba" className="object-cover object-left-top" height={200} width={400} loading = 'lazy' />
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+      </>
+    );
+  };
+
+  return (
+    <>
+      <HeaderBlock />
+    </>
+  );
+};
+
+export default Header;
