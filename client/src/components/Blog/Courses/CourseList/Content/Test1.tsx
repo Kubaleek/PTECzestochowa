7ea432945b3b827f95cc7a457476715732b88260
Kubalek <kuba.krol.ldz@gmail.com>
2024-08-29 15:45:51 +0200

    ulepszenie kursów początki

--- client/src/components/Blog/Courses/CourseList/Content/Test1.tsx
+++ client/src/components/Blog/Courses/CourseList/Content/Test1.tsx
@@ -1,5 +1,17 @@
 export default function Test1() {
     return (
-        <div>Test</div>
+        <div className="bg-[#f5f1ec] flex flex-col md:flex-row justify-between gap-1 border-2 border-[#333]/25 p-2 shadow-lg">
+          <h3 className="text-xl font-bold text-black gap-2 text-pretty leading-relaxed items-center flex place-items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-5 w-5 lucide lucide-book-open-check">
+              <path d="M8 3H2v15h7c1.7 0 3 1.3 3 3V7c0-2.2-1.8-4-4-4Z" />
+              <path d="m16 12 2 2 4-4" />
+              <path d="M22 6V3h-6c-2.2 0-4 1.8-4 4v14c0-1.7 1.3-3 3-3h7v-2.3" />
+            </svg>
+            Szkolenia
+            <span className="bg-green-700 text-white px-2 py-1 text-xs rounded-3xl">
+              0
+            </span>
+          </h3>
+        </div>
     );
 }
