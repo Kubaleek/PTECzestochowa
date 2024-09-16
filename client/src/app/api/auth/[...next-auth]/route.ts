109f8241748641339dd2fa1dae205ea32cb49fd8
danyPL <renatasiwiec71@gmail.com>
2024-09-16 20:55:23 +0200

    Gotowy system (wymagana naprawa 1 bledu)

---
+++ client/src/app/api/auth/[...next-auth]/route.ts
@@ -0,0 +1,5 @@
+
+import NextAuth from "next-auth/next";
+import { options } from "../../../../../auth";
+
+export default NextAuth(options);
--- client/src/lib.ts
+++ client/src/lib.ts
@@ -33,7 +33,7 @@ export async function login(formData: FormData) {
   cookies().set("session", session, { expires, httpOnly: true });
 }
 
-export async function logout() {
+export async function lgoout() {
   // Destroy the session
   cookies().set("session", "", { expires: new Date(0) });
 }
