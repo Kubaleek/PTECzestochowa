51414a352df15a52f10a6f668e316a6a0c9bb36d
Kuba <kuba.krol.ldz@gmail.com>
2024-10-21 16:13:13 +0200

    poprawki i ustawienie proxy

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,20 +1,35 @@
 /** @type {import('next').NextConfig} */
 
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.BACKEND_URL}/api/:path*`,
+      },
+    ];
+  },
+  
   distDir: 'build',
   reactStrictMode: true,
-  webpack(config) {
-    config.optimization.splitChunks = {
-      chunks: 'all',
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.optimization.splitChunks = {
+        chunks: 'all',
+      };
+    }
+
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
     };
 
     return config;
   },
-  experimental: {
-    missingSuspenseWithCSRBailout: false, 
-    workerThreads: false,
-    cpus: 1,
-  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
