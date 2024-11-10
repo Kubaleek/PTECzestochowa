7bc2e15461997c77a5b4cb5cfe41323bd7efb3c7
Kuba <kuba.krol.ldz@gmail.com>
2024-11-10 20:31:29 +0100

    .

--- client/src/components/Cookies/utils/cookie.tsx
+++ client/src/components/Cookies/utils/cookie.tsx
@@ -2,6 +2,8 @@
 
 import { cookies } from "next/headers";
 
-export const setServerCookie = () => {
-  cookies().set('privacyAccepted', 'true', { expires: Date.now() + 365 * 24 * 60 * 60 * 1000 });
+export const setServerCookie = async () => {
+  await cookies().set('privacyAccepted', 'true', { 
+    expires: Date.now() + 365 * 24 * 60 * 60 * 1000 
+  });
 };
