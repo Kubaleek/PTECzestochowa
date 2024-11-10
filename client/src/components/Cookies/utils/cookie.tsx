eb2df577a1ae1bd3cdf4a7c0f0ccda13f4aa1688
Kuba <kuba.krol.ldz@gmail.com>
2024-11-10 21:24:21 +0100

    przywrócenie

--- client/src/components/Cookies/utils/cookie.tsx
+++ client/src/components/Cookies/utils/cookie.tsx
@@ -3,9 +3,7 @@
 import { cookies } from "next/headers";
 
 export const setServerCookie = async () => {
-  const cookieStore = await cookies(); // Await the cookies promise first
-  await cookieStore.set('privacyAccepted', 'true', {
-    expires: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000), // Set expiration date
+  await cookies().set('privacyAccepted', 'true', { 
+    expires: Date.now() + 365 * 24 * 60 * 60 * 1000 
   });
 };
-
